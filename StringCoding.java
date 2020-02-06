80736f9a939a5d336e0bb153b9b260ae086833e9
Xin Liu <xinliu253@yahoo.com>
2020-02-06 18:02:40 -0500

    String coding

--- StringCoding.java
+++ StringCoding.java
@@ -1,9 +1,77 @@
-
+import java.util.*;
 public class StringCoding {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		String pa = "(()())";
+		System.out.println(isValidRec(pa));
+		
+		
+		
 	}
+	/*Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
+	An input string is valid if:
+	
+	Open brackets must be closed by the same type of brackets.
+	Open brackets must be closed in the correct order.
+	Note that an empty string is also considered valid.
+	 * */
+	public static boolean isValid(String s) {
+        if(s.length() == 0)
+            return true;
+        if(s.length()%2 != 0)
+            return false;
+        var stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i ++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '[' )
+                stack.push(c);
+            else{
+                if(stack.isEmpty())
+                    return false;
+                char temp = stack.pop();
+                if(c == ']' && temp != '[' || c == '}' && temp != '{' || c == ')' && temp != '('){
+                        return false;
+                }   
+            }
+        }
+        if(!stack.isEmpty())
+            return false;
+        return true;
+    }
+	//only checks "()"
+	public static boolean isValidLoop(String s) {
+		if(s.isBlank())
+			return true;
+		int p = 0;
+		for(int i = 0; i < s.length(); i ++) {
+			if(p < 0)
+				return false;
+			if(s.charAt(i) == '(')
+				p++;
+			else 
+				p--;			
+		}
+		return p == 0;
+	}
+	
+	public static boolean isValidRec(String s) {
+		if(s.isBlank())
+			return true; 
+		
+		return check(s,0,0);
+	}
+	
+	public static boolean check(String s, int index, int p) {
+		if(index == s.length())
+			return p == 0;
+		else if(p < 0)
+			return false;	
+		if(s.charAt(index) == '(')
+			p++;
+		else
+			p--;		
+		return check(s,++index,p);
+	}
 }
