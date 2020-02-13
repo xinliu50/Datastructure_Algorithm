06208e6d86068df3adc59fc1868306eb3e2e40ca
Xin Liu <xinliu253@yahoo.com>
2020-02-13 01:46:43 -0500

    more String

---
+++ Interview.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+import static java.util.stream.Collectors.*;
+import static java.util.Map.Entry.*;
+
+public class Interview {
+	public static void main(String[] args) {
+//		var map = new HashMap<String, Integer>();
+//		map.put("he", 12);
+//		map.put("sfs", 214);
+//		map.put("cdc", 11);
+//		map.put("ss", 432);
+//		map.put("hffe", 131);
+//		map.put("ff", 31);
+//		map.put("f", 2552);
+//		map.put("we", 12);
+//		System.out.println(map);
+//		Map<String,Integer> sort = map
+//				.entrySet()
+//				.stream()
+//				.sorted(Collections.reverseOrder(comparingByKey()))
+//				.collect(
+//					toMap(e->e.getKey(), e->e.getValue(),(e1,e2)->e2,LinkedHashMap::new)
+//						);
+//		System.out.println(sort);
+//		String [] s = {"232","sfaf","fgg"};
+//		var set = new HashSet<String>(Arrays.asList(s));
+//		System.out.println(set);
+		String a = "sfsfsf";
+		a += ".";
+		System.out.println(a);
+	}
+
+}
--- StringCoding.java
+++ StringCoding.java
@@ -1021,6 +1021,65 @@ public class StringCoding {
 	          });
 	              return logs;
 	       }
+	     /*Given a paragraph and a list of banned words, return the most frequent 
+	      * word that is not in the list of banned words.  It is guaranteed there 
+	      * is at least one word that isn't banned, and that the answer is unique.
+		Words in the list of banned words are given in lowercase, and free of 
+		punctuation.  Words in the paragraph are not case sensitive.  
+		The answer is in lowercase.
+		Example:
+		
+		Input: 
+		paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+		banned = ["hit"]
+		Output: "ball"
+		Explanation: 
+		"hit" occurs 3 times, but it is a banned word.
+		"ball" occurs twice (and no other word does), so it is the most frequent 
+		non-banned word in the paragraph. 
+		Note that words in the paragraph are not case sensitive,
+		that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+		and that "hit" isn't the answer even though it occurs more because it is 
+		banned.
+		Note:
+		
+		1 <= paragraph.length <= 1000.
+		0 <= banned.length <= 100.
+		1 <= banned[i].length <= 10.
+		The answer is unique, and written in lowercase (even if its occurrences 
+		in paragraph may have uppercase symbols, and even if it is a proper noun.)
+		paragraph only consists of letters, spaces, or the punctuation symbols !?',;.
+		There are no hyphens or hyphenated words.
+		Words only consist of letters, never apostrophes or other punctuation symbols.*/
+	     public static String mostCommonWord(String paragraph, String[] banned) {
+	         paragraph = paragraph.toLowerCase();
+	         paragraph += ".";
+	         char[] str = paragraph.toCharArray();
+	         var map = new HashMap<String,Integer>();
+	         var set = new HashSet<String>(Arrays.asList(banned));
+	         var temp = new StringBuilder();
+	         var max = 0;
+	         var ans = "";
+	         for(int i = 0; i < str.length; i ++){
+	             if(Character.isLetter(str[i])){
+	                 temp.append(str[i]);
+	             }
+	             else{
+	                 if(temp.length() != 0){
+	                     String newstr = temp.toString();
+	                     if(!set.contains(newstr)){
+	                         map.put(newstr, map.getOrDefault(newstr,0)+1);
+	                         if(max < map.get(newstr)){
+	                             ans = newstr;
+	                             max = map.get(newstr);
+	                         }
+	                     }
+	                 }
+	                 temp.setLength(0);
+	             }
+	         }
+	         return ans;
+	     }
 }
 class TreeNode {
 	   int val;
